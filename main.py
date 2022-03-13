ea5896c0d75e7a6737fd48c621346fb3860b05a9
RitvikIsGoated <70490029+Ritcuh@users.noreply.github.com>
2022-03-12 16:36:44 -0800

    beginner level 1 should work
    might be some issues, but will fix as we move forward

--- main.py
+++ main.py
@@ -11,9 +11,13 @@ def index():
     return render_template("index.html")
 
 
-@app.route('/level1')
+@app.route('/level1/')
 def level1():
     return render_template("level1.html")
+
+@app.route('/level2/')
+def level2():
+    return render_template("level2.html")
 
 
 # runs the application on the development server
