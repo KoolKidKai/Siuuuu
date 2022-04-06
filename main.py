955fd821d9360727f3942b1fe959adb34d1b5d55
RitvikIsGoated <70490029+Ritcuh@users.noreply.github.com>
2022-04-06 12:43:03 -0700

    layout fix + main

--- main.py
+++ main.py
@@ -19,6 +19,14 @@ def page_not_found(e):
     # note that we set the 404 status explicitly
     return render_template('404.html'), 404
 
+@app.route('/level1')
+def level1():
+    return render_template("level1.html")
+
+@app.route('/level2')
+def level2():
+    return render_template("level2.html")
+
 
 if __name__ == "__main__":
     # runs the application on the repl development server
