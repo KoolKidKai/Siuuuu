ff00af72d963eaed4a7befc25563734797e5b40e
RitvikIsGoated <70490029+Ritcuh@users.noreply.github.com>
2022-04-06 12:37:42 -0700

    update

--- main.py
+++ main.py
@@ -1,34 +1,25 @@
-# import "packages" from flask
-from flask import Flask, render_template
+from flask import render_template
 from __init__ import app
+
 
 from cruddy.app_crud import app_crud
 from cruddy.app_crud_api import app_crud_api
-
 
 app.register_blueprint(app_crud)
 app.register_blueprint(app_crud_api)
 
 
-# create a Flask instance
-app = Flask(__name__)
-
-
-# connects default URL to render index.html
 @app.route('/')
 def index():
     return render_template("index.html")
 
 
-@app.route('/level1/')
-def level1():
-    return render_template("level1.html")
-
-@app.route('/level2/')
-def level2():
-    return render_template("level2.html")
+@app.errorhandler(404)
+def page_not_found(e):
+    # note that we set the 404 status explicitly
+    return render_template('404.html'), 404
 
 
-# runs the application on the development server
 if __name__ == "__main__":
-    app.run(debug=True)
+    # runs the application on the repl development server
+    app.run(debug=True, port="5222")
