b2af3eba746ee9251aafc0708f21c97e31845286
RitvikIsGoated <70490029+Ritcuh@users.noreply.github.com>
2022-04-06 17:52:06 -0700

    login page done login required goodie

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from flask import render_template
+from flask import render_template, redirect, url_for, request
 from __init__ import app
+from flask_login import login_required
 
 
 from cruddy.app_crud import app_crud
@@ -9,23 +10,38 @@ app.register_blueprint(app_crud)
 app.register_blueprint(app_crud_api)
 
 
-@app.route('/')
+
+@app.route("/home")
 def index():
     return render_template("index.html")
 
+# Route for handling the login page logic
+@app.route('/', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != 'SIUUU' or request.form['password'] != 'coding':
+            error = 'Invalid Credentials. Please try again.'
+        else:
+            return redirect(url_for('index'))
+    return render_template('login.html', error=error)
 
-@app.errorhandler(404)
-def page_not_found(e):
-    # note that we set the 404 status explicitly
-    return render_template('404.html'), 404
+#@app.errorhandler(404)
+#def page_not_found(e):
+#    # note that we set the 404 status explicitly
+#    return render_template('404.html'), 404
 
+@login_required
 @app.route('/level1')
 def level1():
     return render_template("level1.html")
 
+
+@login_required
 @app.route('/level2')
 def level2():
     return render_template("level2.html")
+
 
 
 if __name__ == "__main__":
