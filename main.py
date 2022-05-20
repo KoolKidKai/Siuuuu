476746993dddcde152f109bbb076d8d20c68987f
Ritvik-Keerthi <89219486+Ritvik-Keerthi@users.noreply.github.com>
2022-05-20 11:42:29 -0700

    logout button fully functioning

--- main.py
+++ main.py
@@ -23,7 +23,9 @@ def login():
             return render_template('index.html')
     return render_template('login.html', error=error)
 
-
+@app.route('/home', methods=['GET', 'POST'])
+def index():
+    return render_template('index.html')
 
 
 
