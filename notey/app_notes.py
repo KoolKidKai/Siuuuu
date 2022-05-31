8e76ab176aa6c09ae5f19f4b9fa8b855f90f8587
RitvikIsGoated <70490029+Ritcuh@users.noreply.github.com>
2022-05-31 13:07:52 -0700

    add notes

--- notey/app_notes.py
+++ notey/app_notes.py
@@ -3,24 +3,20 @@ from flask import Blueprint, render_template, request, redirect, url_for
 from flask_login import login_required, current_user
 from cruddy.query import user_by_id
 from cruddy.model import Notes
+from uploady.app_upload import upload_save
 
 # blueprint defaults https://flask.palletsprojects.com/en/2.0.x/api/#blueprint-objects
 app_notes = Blueprint('notes', __name__,
                       url_prefix='/notes',
-                      template_folder='/cruddy',
+                      template_folder='templates/notey/',
                       static_folder='static',
                       static_url_path='static')
 
 
-@app_notes.route('/notes')
-@login_required
-def notes():
+def notes_display(uo):  # uo is user object
     # defaults are empty, in case user data not found
     user = ""
     list_notes = []
-
-    # grab user database object based on current login
-    uo = user_by_id(current_user.userID)
 
     # if user object is found
     if uo is not None:
@@ -31,19 +27,41 @@ def notes():
             list_notes.append(note)  # prepare note list for render_template
         if list_notes is not None:
             list_notes.reverse()
-    # render user and note data in reverse chronological order(display latest notes rec on top)
-    return render_template('../cruddy/notes.html', user=user, notes=list_notes)
+    # render user and note data in reverse chronological order
+    return render_template('notes.html', user=user, notes=list_notes)
 
+
+@app_notes.route('/')
+@login_required
+def notes():
+    # grab user database object based on current login
+    uo = user_by_id(current_user.userID)
+    return notes_display(uo)
+
+
+@app_notes.route('/notes_select<userID>')
+@login_required
+def notes_select(userID):
+    # grab user database object based on current login
+    uo = user_by_id(userID)
+    return notes_display(uo)
 
 # Notes create/add
-@app_notes.route('/create/', methods=["POST"])
+@app_notes.route('/note_creator/', methods=["POST"])
 @login_required
-def create():
+def note_creator():
     """gets data from form and add to Notes table"""
     if request.form:
         # construct a Notes object
+        fo = request.files['filename']
+        if len(fo.filename) > 0:
+            filename = upload_save(fo)
+        else:
+            filename = ""
         note_object = Notes(
-            request.form.get("notes"), current_user.userID
+            current_user.userID,
+            request.form.get("notes"),
+            filename
         )
         # create a record in the Notes table with the Notes object
         note_object.create()
